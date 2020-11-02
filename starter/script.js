d5391d5f67e735860650aa87d3ba8bee34ad45cc
sahinyasasi <sahinya.slc@gmail.com>
2020-11-02 11:40:17 +0530

    first commit

---
+++ starter/script.js
@@ -0,0 +1,11 @@
+let js = 'amazing';
+if (js === 'amazing') alert('Hello Der!');
+
+console.log(40 + 8 - 23);
+console.log("jonas");
+//console.log("jonas");
+let firstname = "nick";
+console.log(firstname);
+let $function = 27;
+let danger = 'danger';
+console.log(true);
