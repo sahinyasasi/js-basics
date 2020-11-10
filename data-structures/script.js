4487d06565474ff0aaf380da6bc426832b1ecfd5
sahinyasasi <sahinya.slc@gmail.com>
2020-11-10 15:36:58 +0530

    destructing objects

--- data-structures/script.js
+++ data-structures/script.js
@@ -39,3 +39,23 @@ const [starter, maincourse] = restaurant.order(2, 0);
 console.log(starter, maincourse);
 const [p = 1, q = 1, j = 1, k] = [8, 9];
 console.log(p, q, j, k);
+const { name, openingHours } = restaurant;
+console.log(name, openingHours);
+const { name: restaruantname, openingHours: hours } = restaurant;
+console.log(restaruantname, hours);
+console.log(hours);
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+let a = 23;
+let b = 10;
+const obj = {
+    a: 10,
+    b: 11,
+    c: 12,
+};
+({ a, b } = obj);
+console.log(a, b);
+const arrat = [1, 2, 3];
+const newarr = [1, 2, ...arrat];
+console.log(newarr);
+console.log(...newarr);
