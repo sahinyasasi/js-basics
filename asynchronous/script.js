7892e58ab38138e1f4eddd24fadae13064b7515d
sahinyasasi <sahinya.slc@gmail.com>
2020-12-03 11:50:02 +0530

    promise.race

--- asynchronous/script.js
+++ asynchronous/script.js
@@ -381,7 +381,7 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
     return response.json();
   });
 };
-const get3Countries = async function (c1, c2, c3) {
+/*const get3Countries = async function (c1, c2, c3) {
   try {
     const [data1] = await getJSON(
       `https://restcountries.eu/rest/v2/name/${c1}`
@@ -400,9 +400,32 @@ const get3Countries = async function (c1, c2, c3) {
       getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
     ]);
     console.log(data);
-    console.log(data.map(d => d[0].capital));*/
+    console.log(data.map(d => d[0].capital));
   } catch (err) {
     console.error(err.message);
   }
 };
-get3Countries('portugal', 'canada', 'tanzania');
+get3Countries('portugal', 'canada', 'tanzania');*/
+/*(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.eu/rest/v2/name/italy`),
+    getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
+    getJSON(`https://restcountries.eu/rest/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+*/
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
+  timeout(0.01),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
