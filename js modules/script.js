e698ca8252930abdd21776fa066601c945d86ef3
sahinyasasi <sahinya.slc@gmail.com>
2020-12-04 12:28:39 +0530

    module pattern

--- js modules/script.js
+++ js modules/script.js
@@ -4,3 +4,32 @@ addToCart('soap', 2);
 console.log(price);*/
 import * as ShoppingCart from './shoppingCart.js';
 ShoppingCart.addToCart('soap', 2);
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
